507b983c6147da2a57097c5955b55f7d447201f7
Le Thang <thanglpqe17122@fpt.edu.vn>
2024-09-29 17:47:29 +0700

    Tab Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack screenOptions={{
+      headerShown: false
+    }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
