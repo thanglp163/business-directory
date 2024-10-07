2fac00b326268a52fbda1c732146ce091daaf201
Le Thang <thanglpqe17122@fpt.edu.vn>
2024-10-07 08:44:35 +0700

    set up FireBase and Home slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function home() {
   return (
@@ -8,7 +9,7 @@ export default function home() {
       {/* Header */}
         <Header />
       {/* Slider */}
-
+        <Slider/>
       {/* Category */}
 
       {/* Popular Business List */}
---
+++ components/Home/Slider.jsx
@@ -0,0 +1,51 @@
+import { View, Text, Image, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { db } from './../../configs/FirebaseConfig';
+import { collection, getDocs, query } from 'firebase/firestore';
+
+export default function Slider() {
+    
+    const [sliderList, setSliderList] = useState([]);
+
+    useEffect (() => {
+        GetSliderList();
+    }, []);
+
+    const GetSliderList = async () => {
+        setSliderList([]);
+        const q = query(collection(db, 'Slider'));
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+            setSliderList(prev => [...prev, doc.data()]);
+        })
+    }
+  return (
+    <View>
+      <Text style={{
+        fontFamily:'outfit-medium',
+        fontSize:20,
+        paddingLetft:20,
+        paddingTop:20,
+        marginBottom:5
+      }}>#Special for you
+      </Text>
+
+      <FlatList 
+        data={sliderList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={{paddingLeft:20}}
+        renderItem={({item, index}) => (
+            <Image source={{uri: item.imageUrl}}
+                style={{
+                    width: 360,
+                    height: 160
+                }}
+            />
+        )}
+      />
+    </View>
+  )
+}
