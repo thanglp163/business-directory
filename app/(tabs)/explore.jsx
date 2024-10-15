56a9e0d5dbe4ad3385146c62a6f500e71c3a005f
thanglp163 <lethangtty2018@gmail.com>
2024-10-15 23:40:33 +0700

    update: Explore Page show category

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -1,10 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Colors } from "../../constants/Colors";
+import Category from "../../components/Home/Category";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../configs/FirebaseConfig";
+
 
 export default function explore() {
+
+  const [businessList, setBusinessList] = useState([]);
+  const GetBusinessByCategory = async (category) => {
+    const q = query(collection(db, 'BusinessList'), where('category', '==', category))
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data())
+      setBusinessList(prev => [...prev,{id:doc.id, ...doc.data()}])
+    })
+  }
   return (
-    <View>
-      <Text>explore</Text>
+    <View
+      style={{
+        padding: 20,
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 30,
+        }}
+      >
+        Explore More
+      </Text>
+      {/* Search Bar */}
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 10,
+          alignItems: "center",
+          backgroundColor: "#fff",
+          padding: 10,
+          marginVertical: 10,
+          marginTop: 15,
+          borderRadius: 8,
+          borderWidth: 1,
+          borderColor: Colors.PRIMARY
+        }}
+      >
+        <Ionicons name="search" size={24} color={Colors.PRIMARY} />
+        <TextInput
+          placeholder="Search..."
+          style={{
+            fontFamily: "outfit",
+            fontSize: 16,
+          }}
+        />
+      </View>
+      {/* Category */}
+          <Category
+            explore={true}
+            onCategorySelect={(category) => GetBusinessByCategory(category)}
+          />
+      {/* Business List */}
+      
     </View>
-  )
+  );
 }
--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -6,7 +6,7 @@ import { db } from './../../configs/FirebaseConfig'
 import CategoryItem from './CategoryItem'
 import { useRouter } from 'expo-router'
 
-export default function Category() {
+export default function Category({explore = false, onCategorySelect}) {
   
   const [categoryList, setCategoryList] = useState([]);
   const router = useRouter();
@@ -25,9 +25,19 @@ export default function Category() {
     })
   } 
 
+  const onCategoryPressHandler = (item) => {
+    if(!explore)
+    {
+      router.push('/businesslist/' + item.name)
+    }
+    else{
+      onCategorySelect(item.name)
+    }
+  }
+
   return (
     <View>
-        <View style={{padding:20, display:'flex', 
+        {!explore && <View style={{padding:20, display:'flex', 
         flexDirection:'row', justifyContent:'space-between', 
         marginTop:10}}>
             <Text 
@@ -37,7 +47,7 @@ export default function Category() {
                     Category
             </Text>
             <Text style={{color:Colors.PRIMARY, fontFamily:'outfit-medium'}}>View All</Text>
-        </View>
+        </View>}
         <FlatList 
           data={categoryList}
           horizontal={true}
@@ -47,7 +57,8 @@ export default function Category() {
             <CategoryItem 
               category={item} 
               key={index}
-              onCategoryPress={(category) => router.push('/businesslist/' + item.name)}
+              onCategoryPress={(category) => 
+                onCategoryPressHandler(item)}
             />
         )}
         />
