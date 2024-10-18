024e98a59843c9ef62e47ddb629091564328c7db
Le Thang <thanglpqe17122@fpt.edu.vn>
2024-10-18 15:50:41 +0700

    update: add business to fire base

--- app/business/add-business.jsx
+++ app/business/add-business.jsx
@@ -1,22 +1,26 @@
-import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, Image, TouchableOpacity, TextInput, ActivityIndicator, ToastAndroid } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "expo-router";
 import { Colors } from "../../constants/Colors";
 import * as ImagePicker from 'expo-image-picker';
 import RNPickerSelect from 'react-native-picker-select';
-import { collection, getDocs, query } from "firebase/firestore";
-import { db } from "../../configs/FirebaseConfig";
+import { collection, doc, getDocs, query, setDoc } from "firebase/firestore";
+import { db, storage } from "../../configs/FirebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useUser } from "@clerk/clerk-expo";
 
 export default function AddBusiness() {
     const navigation = useNavigation();
     const [image, setImage] = useState(null);
     const [categoryList, setCategoryList] = useState([]);
+    const {user} = useUser();
     const [name, setName] = useState();
     const [address, setAddress] = useState();
     const [contact, setContact] = useState();
     const [website, setWebsite] = useState();
     const [about, setAbout] = useState();
     const [category, setCategory] = useState();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         navigation.setOptions({
@@ -48,6 +52,41 @@ export default function AddBusiness() {
                 value:(doc.data()).name
             }])
         })
+    }
+
+    const onAddNewBusiness = async () => {
+        setLoading(true);
+        const fileName = Date.now().toString() + '.jpg';
+        const resp = await fetch(image);
+        const blob = await resp.blob();
+        const imageRef = ref(storage, 'business-app/' + fileName);
+
+        uploadBytes(imageRef, blob).then((snapshot) => {
+            console.log('file uploaded successfully')
+        }).then(resp => {
+            getDownloadURL(imageRef).then(async (downloadUrl) => {
+                console.log(downloadUrl);
+                saveBusinessDetail(downloadUrl)
+            })
+        })
+        setLoading(false);
+    }
+
+    const saveBusinessDetail = async (imageUrl) => {
+        await setDoc(doc(db, 'BusinessList', Date.now().toString()),{
+            name: name,
+            contact: contact,
+            address: address,
+            website: website,
+            about: about,
+            category: category,
+            username: user?.fullName,
+            userEmail: user?.primaryEmailAddress?.emailAddress,
+            userImage: user?.imageUrl,
+            imageUrl: imageUrl
+        })
+        setLoading(false);
+        ToastAndroid.show('New Business Added...', ToastAndroid.LONG)
     }
 
     return (
@@ -168,19 +207,26 @@ export default function AddBusiness() {
                 </View>
             </View>
             
-            <TouchableOpacity style={{
-                padding: 15,
-                backgroundColor: Colors.PRIMARY,
-                borderRadius: 15,
-                marginTop: 20,
-            }}>
+            <TouchableOpacity 
+                disabled={loading}
+                style={{
+                    padding: 15,
+                    backgroundColor: Colors.PRIMARY,
+                    borderRadius: 15,
+                    marginTop: 20,
+                }}
+                onPress={ () => onAddNewBusiness()}
+            >
+            {loading?
+                <ActivityIndicator size={'large'} color={'#fff'} />
+                :
                 <Text style={{
                     textAlign: 'center',
                     fontFamily: 'outfit-medium',
                     color: '#fff'
                 }}>Add New Business</Text>
+            }
             </TouchableOpacity>
-
         </View>
-    );
+    )
 }
